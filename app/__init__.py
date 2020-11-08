40bd9ae5004f49d7e3614ad9ec025a8f6bf2bced
Zachary N <zachary.nack@icloud.com>
2020-11-08 13:19:45 -0800

    flask skeleton

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,26 @@
+import os
+
 from flask import Flask
 
-app = Flask(__name__)
+def create_app(test_config=None):
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path)
+    )
 
-from app import routes
+    if test_config is None:
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        app.config.from_mapping(test_config)
+    
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
 
+    @app.route('/hello')
+    def hello():
+        return 'Hello, world'
+
+    return app
